38dd410bb1f8b1aa74d4dfab8b7b2d9f3829818d
Arthur <arthur.1234.5678.alves@gmail.com>
2025-12-03 23:22:16 -0300

    updated!

--- middleware.js
+++ middleware.js
@@ -1,22 +1,24 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const country = request.geo?.country || "UNKNOWN";
+    const country = request.geo?.country || "UNKNOWN";
 
-  // Permite acesso para os EUA
-  if (country === "BR") {
-    return NextResponse.next();
-  }
+    // Permite acesso para os EUA
+    if (country === "BR") {
+        return NextResponse.next();
+    }
 
-  // Redireciona para uma página de bloqueio
-  const url = request.nextUrl.clone();
-  url.pathname = "/blocked";
-  return NextResponse.rewrite(url);
+    // Redireciona para uma página de bloqueio
+    const url = request.nextUrl.clone();
+    url.pathname = "/blocked";
+    return NextResponse.rewrite(url);
 
-  // Se preferir bloquear com erro 403:
-  // return new NextResponse("Acesso bloqueado", { status: 403 });
+    console.log("Geo detectado:", request.geo);
+    console.log("País detectado:", request.geo?.country);
+    // Se preferir bloquear com erro 403:
+    // return new NextResponse("Acesso bloqueado", { status: 403 });
 }
 
 export const config = {
-  matcher: "/:path*", // aplica em todas as rotas
+    matcher: "/:path*", // aplica em todas as rotas
 };
