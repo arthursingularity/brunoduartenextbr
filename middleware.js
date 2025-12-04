28b71016285c5cd5ec5cd0c738ee47511902938c
Arthur <arthur.1234.5678.alves@gmail.com>
2025-12-03 23:42:58 -0300

    updated!

--- middleware.js
+++ middleware.js
@@ -1,23 +1,21 @@
 import { NextResponse } from "next/server";
 
-export const runtime = "edge"; // força edge (muito importante)
+export const runtime = "edge";
 
 export function middleware(request) {
-    console.log("Geo detectado:", request.geo);
-    console.log("País detectado:", request.geo?.country);
-    console.log("IP detectado:", request.ip);
+    const country = request.geo?.country;
 
-    const country = request.geo?.country || "BR";
-
-    if (country === "BR") {
-        return NextResponse.next();
+    // Se não for BR, bloqueia
+    if (country !== "BR") {
+        const url = request.nextUrl.clone();
+        url.pathname = "/blocked"; // página de bloqueio
+        return NextResponse.rewrite(url);
     }
 
-    const url = request.nextUrl.clone();
-    url.pathname = "/blocked";
-    return NextResponse.rewrite(url);
+    // Se for BR, libera o acesso
+    return NextResponse.next();
 }
 
 export const config = {
-    matcher: "/:path*",
+    matcher: "/:path*", // aplica em todas as rotas
 };
