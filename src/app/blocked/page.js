f6b0030e4aa7acc22dced40f43713330867852a7
Arthur <arthur.1234.5678.alves@gmail.com>
2025-12-04 00:06:44 -0300

    code updated!

--- src/app/blocked/page.js
+++ src/app/blocked/page.js
@@ -1,8 +1,13 @@
 export default function BlockedPage() {
-    return (
-      <div className="text-center text-white">
-        <h1>Acess Restricted</h1>
-        <p>This website is only available for users from Brazil.</p>
+  return (
+    <div className="flex items-center justify-center h-screen p-6 bg-white">
+      <div className="max-w-md w-full text-center p-8">
+        <h1 className="text-4xl font-bold text-gray-800 mb-2">Access Restricted</h1>
+        <p className="text-gray-600 font-regular text-xl">
+          This website is only available for users from <span className="font-bold">Brazil</span>.
+        </p>
       </div>
-    );
-  }
+    </div>
+
+  );
+}
